09a15b0a63810447fe3d10cd7c2b179386c3ee9f
ClintonYounge <clintonyounge18@gmail.com>
2023-02-14 22:26:00 -0400

    Day 2- Update html css and js
    -fixed menu.
    -completed basic about page.
    -completed to be optimized mobile versions.
    -fix all links for mobile version

--- about page/about-mobile-menu.js
+++ about page/about-mobile-menu.js
@@ -2,22 +2,25 @@ const menuBtn = document.querySelector('.about-menu-icon');
 const menu = document.getElementById('theMenu');
 const xBtn = document.getElementById('xBtn');
 const navLinks = document.querySelectorAll('.navs');
-const introductionSection = document.querySelector('.introduction');
-const mainProg = document.querySelector('.main-program');
-const featuredSection = document.querySelector('.featured')
+const introductionSection = document.querySelector('.about-introduction');
+const mainProg = document.querySelector('.about-description');
+const featuredSection = document.querySelector('.past-events');
+const footer = document.querySelector('footer');
 
 function openMenu() {
   menu.style.display = 'block';
-  introductionSection.style.display = 'none'
-  mainProg.style.display = 'none'
-  featuredSection.style.display = 'none'
+  introductionSection.style.display = 'none';
+  mainProg.style.display = 'none';
+  featuredSection.style.display = 'none';
+  footer.style.display = 'none';
 }
 
 function closeMenu() {
   menu.style.display = 'none';
-  introductionSection.style.display = 'block'
-  mainProg.style.display = 'block'
-  featuredSection.style.display = 'block'
+  introductionSection.style.display = 'block';
+  mainProg.style.display = 'block';
+  featuredSection.style.display = 'block';
+  footer.style.display = 'block';
 }
 
 menuBtn.addEventListener('click', openMenu);
--- js/speakers.js
+++ js/speakers.js
@@ -1,4 +1,3 @@
-const showAllSpeakers = document.getElementById('showAllSpeakers');
 const moreSpeakers = document.getElementById('moreSpeakers');
 const speakers = document.getElementById('speakers');
 const otherSpeakers = document.getElementById('otherSpeakers');
@@ -8,31 +7,31 @@ const guidesArray = [
     name: 'Mikhail Rodrigues',
     picture: 'images/Guyanese-Critics.jpg',
     profession: 'Social Media Influencer',
-    description: 'Excellent knowledge of JavaScript/TypeScript, HTML and CSS. Provides mentorship and supervise developers',
+    description: 'Journalist, groundman for the truth, a sensation to the people. The Guyanese Critics.',
   },
   {
     name: 'Clinton Younge',
     picture: 'images/Driver.jpg',
     profession: 'Entrepreneur/Driver',
-    description: 'Excellent knowledge of JavaScript/TypeScript, HTML and CSS. Provides mentorship and supervise developers',
+    description: 'Entrepreneur who has his hand in many different fields. Our main driver for the event.',
   },
   {
     name: 'Delvin Adams',
     picture: 'images/Delvin-Adams-Chef.jpg',
     profession: 'Master Chef',
-    description: 'Excellent knowledge of JavaScript/TypeScript, HTML and CSS. Provides mentorship and supervise developers',
+    description: "Master of the kitchen, savior our tastebuds have been waiting for. The chef that'll have you taste Guyana."
   },
   {
     name: 'Poonam Singh',
     picture: 'images/Poonam-Singh.jpg',
     profession: "Guyana's #1 Singer",
-    description: 'Excellent knowledge of JavaScript/TypeScript, HTML and CSS. Provides mentorship and supervise developers',
+    description: 'An ethereal beauty with a voice so mellow. Come listen as she sings her hit single "Guyana".',
   },
   {
     name: 'Ivan Sancho',
     picture: 'images/Ivan-Sancho-Craftsman.jpg',
     profession: 'Master Craftsman',
-    description: 'Excellent knowledge of JavaScript/TypeScript, HTML and CSS. Provides mentorship and supervise developers',
+    description: 'Master of his craft. Ivan Sancho will showcase the skill of craftmaking only a man of his calibur and experience can showcase.',
   },
   {
     name: 'His Excellency, President Irfan Ali',
