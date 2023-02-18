4e9bc98498116e117d6168f1ce733bf7a30f2069
ClintonYounge <clintonyounge18@gmail.com>
2023-02-18 00:15:50 -0400

    Update html and css
    Increase overall responsiveness by utilizing media queries for 4 separate breakpoints

--- about-page/about.js
+++
@@ -1,6 +0,0 @@
-const bottomRight = document.querySelector('.bottom-right p');
-
-const mediaQuery = window.matchMedia('(min-width: 785px)');
-if (mediaQuery.matches) {
-  bottomRight.innerHTML = '2023, some rights reserved. This is a capstone project that shows my current level as it relates to web development.';
-}
