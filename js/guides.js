8ecdb41732685d0adcd1258229b1cb98ac59a4c3
ClintonYounge <clintonyounge18@gmail.com>
2023-02-17 20:07:17 -0400

    Update js and css
    -Update images
    -edit image sizes
    -edit image paths
    -fix eslint and stylint linter errors

--- js/guides.js
+++ js/guides.js
@@ -56,10 +56,3 @@ window.addEventListener('load', (() => {
   guides.innerHTML = content;
   content = '';
 }));
-
-// const bottomRight = document.querySelector('.bottom-right p');
-
-// const mediaQuery = window.matchMedia('(min-width: 785px)');
-// if (mediaQuery.matches) {
-//   bottomRight.innerHTML = '2023, some rights reserved. This is a capstone project that shows my current level as it relates to web development.';
-// }
